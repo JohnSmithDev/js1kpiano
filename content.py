eaf1e6c6527d846c302dc0744b54ed5bac01a48b
john <john@0e56b9e8-9c15-48ba-84e4-773b52758957>
2010-09-05 21:39:29 +0000

    more brochureware and styling improvements
    git-svn-id: svn://sofia/js1kpiano/trunk@8 0e56b9e8-9c15-48ba-84e4-773b52758957

--- content.py
+++ content.py
@@ -2,22 +2,28 @@
    Copyright Menboku Ltd 2010 www.menboku.co.uk
    Licenced under GPL v2"""
 
-import re
+import re, logging
 
 class BrowserDetails:
   def __init__(self, user_agent_string):
     self.user_agent = user_agent_string
-    if user_agent_string.find("Firefox") >=0 :
+    if user_agent_string.find("Chrome") >= 0:
+        self.browser_name = "Chrome"
+        self.browser_family = "Chrome"
+        self.browser_engine = "Webkit"
+    elif user_agent_string.find("Chromium") >= 0:
+        self.browser_name = "Chromium"
+        self.browser_family = "Chrome"
+        self.browser_engine = "Webkit"
+    elif user_agent_string.find("Iron") >= 0:
+        self.browser_name = "Iron"
+        self.browser_family = "Chrome"
+        self.browser_engine = "Webkit"
+    elif user_agent_string.find("Firefox") >=0 :
         self.browser_name = "Firefox"
         self.browser_engine = "Gecko"
     elif user_agent_string.find("Safari") >= 0:
         self.browser_name = "Safari"
-        self.browser_engine = "Webkit"
-    elif user_agent_string.find("Chrome") >= 0:
-        self.browser_name = "Chrome"
-        self.browser_engine = "Webkit"
-    elif user_agent_string.find("Iron") >= 0:
-        self.browser_name = "Iron"
         self.browser_engine = "Webkit"
     elif user_agent_string.find("Opera") >= 0:
         self.browser_name = "Opera"
@@ -31,6 +37,8 @@ class BrowserDetails:
 
     if user_agent_string.find("Linux")>=0:
         self.os_name = "Linux"
+    elif user_agent_string.find("iPhone OS") >= 0:
+        self.os_name = "iOS"
     elif user_agent_string.find("Mac") >= 0:
         self.os_name = "Mac OS"
     elif user_agent_string.find("Windows") >= 0:
@@ -40,7 +48,10 @@ class BrowserDetails:
 
   def is_compatible(self):
     if self.browser_name in ["Firefox", "Safari", "Opera"]:
-      return True
+      if self.os_name == "iOS":
+        return False
+      else:
+        return True
     else:
       return False
 
@@ -53,6 +64,11 @@ def dict_for_django_template(rh):
   else:
     ua = "[No browser User-Agent passed]"
   browser = BrowserDetails(ua)
+  logging.debug("%s / %s / %s <= %s" %
+                (browser.browser_name, browser.browser_engine,
+                 browser.os_name,
+                 ua))
+                 
   return {
     "browser": browser,
     "isCompatible": browser.is_compatible(),
